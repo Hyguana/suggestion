76d40c8bb26158d00a582825e6c6c3f74b57a72a
DANIEL ASSUNCAO <35895330+Hyguana@users.noreply.github.com>
2021-11-29 03:27:48 +0000

    create modules

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,7 +1,9 @@
+import styles from './estiloso.modules.css'
+
 export default function Estiloso(){
     return (
         <div>
-            <h1>Estilo usando Módulo</h1>
+            <h1>Estilo usando CSS Módulos</h1>
         </div>
     )
 }
